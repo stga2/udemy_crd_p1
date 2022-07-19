fae40b26acc635e3f7d8ad2087b6e23090e69ad8
SmeProfessionalDevTeam <108350962+SmeProfessionalDevTeam@users.noreply.github.com>
2022-07-19 14:31:55 +0100

    Nested Routing and Navigation bar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+      <Outlet />
+    </div>
+  );
+};
+
+const Shop = () => {
+  return <h1>Shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
