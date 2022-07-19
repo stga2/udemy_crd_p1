ccdeb38c7e3c903ac57d4d9fe87f4570f032a0ec
SmeProfessionalDevTeam <108350962+SmeProfessionalDevTeam@users.noreply.github.com>
2022-07-19 15:09:55 +0100

    Adding logo

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,16 @@
 import { Component, Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+// import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation.style.scss";
+
 class Navigation extends Component {
   render() {
     return (
       <Fragment>
         <div className="navigation">
           <Link class="logo-container" to="/">
-            <div>Logo</div>
+            <img className="logo" src="/crown.png" />
           </Link>
 
           <div className="nav-links-container">
@@ -15,8 +18,8 @@ class Navigation extends Component {
               SHOP
             </Link>
           </div>
-          <Outlet />
         </div>
+        <Outlet />
       </Fragment>
     );
   }
